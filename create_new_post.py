5a40d7d723eda32f716f659c83c475b2c97c2ec4
rgodinez <cyrik13@yahoo.com>
2016-05-19 05:09:34 -0400

    worked on posting script
    just generates date and passes title into template

--- create_new_post.py
+++ create_new_post.py
@@ -1,7 +1,44 @@
----
-layout: post
-title: ""
-date: YYYY-MM-DD
----
+import sys
+import time
+import os
 
-<!-- Post text goes here -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def writeFile(postTitle):
+	date = time.strftime('%Y-%m-%d')
+	path = os.getcwd() + '\\_posts\\'
+	
+	postFile = open(path + date + '-' + postTitle.replace(' ', '-') + '.md', 'w')
+	
+	postFile.write('---')
+	postFile.write('\nlayout: post')
+	postFile.write('\ntitle: \"' + postTitle + '\"')
+	postFile.write('\ndate: ' + date)
+	postFile.write('\n---')
+	postFile.write('\n')
+	postFile.write('\n<!-- Post text goes here -->')
+	
+	postFile.close()
+
+if __name__ == "__main__":
+	#	Only argument is post title name
+	if len(sys.argv) != 2:
+		print "Missing post title: syntax is python create_new_post.py \"title name\""
+		sys.exit()
+	
+	argument = ''.join(sys.argv[1:])
+	
+	writeFile(argument)
